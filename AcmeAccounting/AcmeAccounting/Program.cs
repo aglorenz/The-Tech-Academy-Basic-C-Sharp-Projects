a313713bae006ddad704d812e1d149a61e43d834
Andrew Lorenz <aglorenz777@gmail.com>
2018-11-29 15:06:22 -0800

    Print message to console that contains a word surrounded by double quotes.

---
+++ AcmeAccounting/AcmeAccounting/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine(@"Remember, we're ""accounting"" on you!");
+        //Console.WriteLine("Remember, we're \"accounting\" on you!"); // Alternate way of printing double quotes
+        Console.Read();
+        }
+    }
