d4b8dba56ad4a824a5b8c6e9b9583366a88378f8
Andrew Lorenz <aglorenz777@gmail.com>
2018-11-29 22:49:13 -0800

    Exercise to program per biz req'ts and demonstrate Boolean logic

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,29 @@
+﻿//
+//  Author: Andrew Lorenz
+//  Date:   11/29/2018
+//  Purpose: Given business requirements, create program to demonstrate use of Boolean logic
+//           Bus Req'ts:  Applicant for insurance  1) is older than 15, 2) no DUI's, 3) no more than 3 speeding tickets 
+
+using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("What is your age?\n>> ");
+            byte age = Convert.ToByte(Console.ReadLine());
+
+            Console.Write("\nHave you ever had a DUI? (true or false)\n>>");
+            bool hadDUI = Convert.ToBoolean(Console.ReadLine());
+
+            Console.Write("\nHow many speeding tickets do you have?\n>>");
+            byte numTickets = Convert.ToByte(Console.ReadLine());
+
+            // True if all 3 conditions are true:  1) is older than 15, 2) no DUI's, 3) no more than 3 speeding tickets 
+            Console.WriteLine("\nQualified?\n>>" + ((age > 15) && (hadDUI == false) && (numTickets <= 3)));
+
+            Console.ReadLine();
+
+
+        }
+    }
