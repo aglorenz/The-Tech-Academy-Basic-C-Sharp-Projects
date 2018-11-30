8a138330b529e22017b739e2bad3dfa0e718abc3
Andrew Lorenz <aglorenz777@gmail.com>
2018-11-29 16:23:02 -0800

    Exploring variables and data types

---
+++ VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a simple program
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemp = -23;
+            char questionMark = '\u2103';
+
+            decimal moneyInBank = 100.5m; // m for decimal
+
+            double heightInCentimeters = 211.2020202092;
+
+            float secondsLeft = 2.62f; // f for float
+
+            short tempOnMars = -341;
+
+            string myName = "Jesse";
+
+            int currentAge = 30;
+            string yearOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+
+            Console.WriteLine(rainingStatus);
+            Console.Read();
+
+        }
+    }
+}
