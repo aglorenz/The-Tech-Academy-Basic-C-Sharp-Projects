bcd021924a3d5fb93561d2c7cfd3f630b39c346d
Andrew Lorenz <aglorenz777@gmail.com>
2018-12-07 11:13:18 -0800

    Try Catch Follow along exercise

---
+++ ExceptionHandlingFollowAlong/ExceptionHandlingFollowAlong/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try // divide two numbers entered by user.  first number / second number
+        {
+            Console.WriteLine("Pick a number");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a second number");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dividing the two....");
+            int numberThree = numberOne / numberTwo;
+            Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+            Console.ReadLine();
+        }
+        // catch error if user enters a non-numeric string
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please type a whole number");
+            return;
+        }
+        // catch error if user enters 0 for second number
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don't divide by zero");
+        }
+        // general exceptions caught here
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        // This section executed even if, for example, there is a return statement contained in other catches.
+        finally
+        {
+            Console.ReadLine();
+        }
+
+
+    }
+}
