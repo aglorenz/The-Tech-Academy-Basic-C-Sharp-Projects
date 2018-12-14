8c066706c193128b800d118f4fb8dd3145869fa6
Andrew Lorenz <aglorenz777@gmail.com>
2018-12-13 16:31:00 -0800

    My First Classes and Methods Exercise.

---
+++ ClassesAndMethodsExercise1/ClassesAndMethodsExercise1/Number.cs
@@ -0,0 +1,31 @@
+﻿// My first class with 3 methods but no data types.
+namespace ClassesAndMethodsExercise1
+{
+    public class Number
+    {
+        // Add 150 to input parameter and return sum
+        public static int Add150To(int addend)
+        {
+            return addend + 150;
+        }
+
+        // subtract 22 from input parameter and return difference
+        public static int Subtract22From(int minuend)
+        {
+            return minuend - 22;
+        }
+
+        // multiply input parameter by 500 and return product
+        public static int MultiplyBy500(int factor)
+        {
+            return factor * 500;
+        }
+
+        // declaring this here means I don't have to do something like
+        // Number myNum = new Number(); in the main program.  I love this!
+        public Number()
+        {
+        }
+
+    }
+}
---
+++ ClassesAndMethodsExercise1/ClassesAndMethodsExercise1/Program.cs
@@ -0,0 +1,36 @@
+﻿
+//  Author: Andrew Lorenz
+//  Date:   12/13/2018
+//  Purpose:    Given requirements: Create a class with 3 methods each of which take 1 input parm and return
+//              an integer.  Each method should do a simple math operation on the input. Put the class in a 
+//              separate .cs file in the application.  Query the user from the console for an integer and 
+//              call each method passing in the user input.  Display the result on the console.
+//                      
+
+using System;
+
+namespace ClassesAndMethodsExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a whole number on which we will perform 3 mathmatic operations \n>> ");
+            int userInt = Convert.ToInt32(Console.ReadLine());
+
+            // Call method to add 150 to user number and display to console
+            Console.WriteLine("\n  " + userInt + " plus  150 = " 
+                + Number.Add150To(userInt).ToString().PadLeft(5));
+
+            // Call method to substract 22 from user number and display to console
+            Console.WriteLine("\n  " + userInt + " minus  22 = " 
+                + Number.Subtract22From(userInt).ToString().PadLeft(5));
+            
+            // Call method to multiplyuser number by 500 and display to console
+            Console.WriteLine("\n  " + userInt + " times 500 = " 
+                + Number.MultiplyBy500(userInt).ToString().PadLeft(5));
+
+            Console.ReadLine();
+        }
+    }
+}
