87e2df066f67609b5e7178b7a75e339513fcc4f7
Andrew Lorenz <aglorenz777@gmail.com>
2018-12-20 19:21:46 -0800

    Moved Shuffle method to the Deck class

--- TwentyOneGameFollowAlong/TwentyOneGameFollowAlong/Deck.cs
+++ TwentyOneGameFollowAlong/TwentyOneGameFollowAlong/Deck.cs
@@ -6,16 +6,11 @@ using System.Threading.Tasks;
 
 namespace TwentyOneGameFollowAlong
 {
-    class Deck
+    public class Deck
     {
         public Deck()
         {
-            //Cards = new List<Card>();
-            //Card cardOne = new Card();
-            //cardOne.Face = "Two";
-            //cardOne.Suit = "Hearts";
-            //Cards.Add(cardOne);
-
+            
             Cards = new List<Card>();
             List<string> Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" };
             List<string> Faces = new List<string>()
@@ -25,7 +20,7 @@ namespace TwentyOneGameFollowAlong
             };
 
             foreach (string face in Faces)
-            {
+            { 
                 foreach (string suit in Suits)
                 {
                     Card card = new Card();
@@ -38,6 +33,25 @@ namespace TwentyOneGameFollowAlong
             }
         }
 
-            public List<Card> Cards { get; set; }
+        public List<Card> Cards { get; set; }
+
+        public void Shuffle(int times = 1)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, Cards.Count);
+                    TempList.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+                Cards = TempList;
+
+            }
+        }
+
     }
 }
