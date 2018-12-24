67448e353e221528056db0f1a2945c71d967ee23
Andrew Lorenz <aglorenz777@gmail.com>
2018-12-23 16:25:54 -0800

    First Inheritance Exercise

---
+++ InheritanceExercise1/InheritanceExercise1/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Author: Andrew Lorenz
+//  Date:   12/23/2018
+//  Purpose:    Given requirements: Create a  class called Person with two properties:  first and last name.  Give the class a 
+//              void method SayName with no parms that writes the person's full name to the console  in format  Name:  [full name]
+//              Create another class Employee and have it inherit from the Person class.  Give the Employee class a property Id of type int
+//              In the main method, instantiate and initializean Employee object with a first name of "Sample", last name "Student"
+using System;
+
+namespace InheritanceExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee  // showing simplified initialization
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+            // Initialize the First and Last name in the superclass Employee
+            // Call the superclass method to print out the Employee first and last name
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
