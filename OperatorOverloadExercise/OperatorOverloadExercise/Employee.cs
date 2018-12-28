dbf3ac4040349e5886e722fc2bd26c65cffbfa23
Andrew Lorenz <aglorenz777@gmail.com>
2018-12-28 13:32:13 -0800

    Overload the "==" operator so it checks if two Employee objects are equal by comparing Id

---
+++ OperatorOverloadExercise/OperatorOverloadExercise/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OperatorOverloadExercise
+{
+    public class Employee : Person, IQuittable
+    {
+        public int Id { get; set; }
+
+        // Overload operator == with this method.
+        public static Boolean operator== (Employee firstEmployee, Employee secondEmployee)
+        {
+            return firstEmployee.Id == secondEmployee.Id ? true : false;
+        }
+
+        // Overload operator != with this method.  It's required to be here because we have overloaded == above
+        public static Boolean operator!= (Employee firstEmployee, Employee secondEmployee)
+        {
+            return firstEmployee.Id != secondEmployee.Id ? true : false;
+        }
+
+        public override void SayName() // override the SayName definition (declared in Person class) and implement it here.
+        {
+            Console.WriteLine("Name: {0} {1}", firstName, lastName);
+        }
+
+        public void Quit() // implement the method from the IQuittable interface
+        {
+            Console.WriteLine("\n\"I like turtles!\", said the polymorphic snowman.");
+            Console.WriteLine("\nThen he said quite quickly, quirkily, and quietly, \"I quit.\"");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OperatorOverloadExercise/OperatorOverloadExercise/Program.cs
@@ -0,0 +1,49 @@
+﻿//  Author: Andrew Lorenz
+//  Date:   12/28/2018
+//  Purpose:    Requirements: Using previous Employee/Person exercise, overload the "==" operatore so it checks
+//              if two Employee objects are equal by comparing their Id property.
+using System;
+
+namespace OperatorOverloadExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee // Instantiate Employee object and initialize first and last name.
+            {
+                firstName = "Andy",
+                lastName = "Lorenz",
+                Id = 1
+            };
+            
+            // Here we have two employees with the same name but different Id 
+            Employee employee2 = new Employee
+            {
+                firstName = "Andy",
+                lastName = "Lorenz",
+                Id = 2
+            };
+
+            Console.WriteLine("\nSince \n\n\tEmployee 1 Id = {0}, and \n\tEmployee 2 Id = {1},", employee1.Id, employee2.Id);
+
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("\nthe employees are the same person.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("\nthe employees are NOT the same person.");
+                Console.ReadLine();
+            }
+
+            IQuittable iquit = new Employee();  // <== polymorphism right here, folks!
+            iquit.Quit();
+
+            //employee.SayName();  // Write the name to the console.
+            //Console.ReadLine();
+
+        }
+    }
+}
