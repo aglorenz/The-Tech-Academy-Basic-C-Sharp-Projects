9ac63fa476a4b17d8a74dca7d6f2749f20cdc6d7
Andrew Lorenz <aglorenz777@gmail.com>
2018-12-30 15:15:10 -0800

    Updated header comments

--- StructureExercise/StructureExercise/Program.cs
+++ StructureExercise/StructureExercise/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ * Author:  Andrew Lorenz
+ * Date:    12/30/2018
+ * Purpose: Demonstrate use of a structure
+ * Req'ts:  Create a struct called Number and give iti the property "Amount" and have it be of data type decimal.
+ *          In the Main() method, create an object of data type Number and assign an amount to it.
+ *          Print this amount to the console.
+ */using System;
 
 namespace StructureExercise
 {
