fdb86971437443fd85e32ff1bd7e88550535dc1b
Andrew Lorenz <aglorenz777@gmail.com>
2024-01-29 17:01:27 -0800

    IEnumerable Demo

---
+++ IEnumerable_Demo/IEnumerable_Demo/IEnums.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IEnumerable_Demo
+{
+    // from https://james-ramsden.com/implement-ienumerable-c/
+
+    // IEnumerable tells the compiler that there's something in the class we 
+    // can iterate through
+    public class IEnums : IEnumerable
+    {
+        public IEnums()   // Constructor
+        {
+            myData = new List<double>(new double[] { 3.4, 1.2, 6.2 });
+        }
+        private List<double> myData;
+
+
+
+        // Here we implement the IEnumerable interface member
+        // This is specific to this class object. This method gets called
+        // when we try to iterate through this class with foreach.
+        public IEnumerator<double> GetEnumerator()
+        {
+            foreach (double val in myData)
+            {
+                // Allows return of a value without breaking from the loop
+                yield return val;
+            }
+        }
+
+        //This method is also needed, but usually you don't need to change it from this.
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerable<double> GetReverseEnumerator()
+        {
+            for (int i = myData.Count - 1; i >= 0; i--)
+            {
+                yield return myData[i];
+            }
+        }
+    }
+}
---
+++ IEnumerable_Demo/IEnumerable_Demo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IEnumerable_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnums myList = new IEnums();
+            List<double> rtnList = new List<double>();
+            
+            // Iterate forward through list of IEnum objects
+            foreach (var num in myList)
+            {
+                // need IEnumerable.GetEnumerator and
+                // IEnumerator<double> to add double value to list
+                rtnList.Add(num);
+            }
+
+            // Simple iteration through a list of doubles created by the above iteration (not objects)
+            
+            foreach (var num in rtnList)
+            {
+                Console.WriteLine(num);
+            }
+
+            // Iterate in reverse order through list of IEnum objects 
+            foreach (var num in
+                myList.GetReverseEnumerator())
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+        }
+    }
+}
